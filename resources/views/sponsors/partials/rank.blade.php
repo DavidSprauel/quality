a0ba68c965c83c2a1cb751c8b930ed7655732b2c
David Sprauel <davidsprauel@MBP-de-David.(none)>
2017-12-19 17:48:09 +0100

    fix sponosrs

--- resources/views/sponsors/partials/rank.blade.php
+++ resources/views/sponsors/partials/rank.blade.php
@@ -1,26 +1,34 @@
 <h1 class="title is-1 title-perso">
     Gold
 </h1>
-@forelse($sponsors->get('gold') as $sponsor)
-    <div class="columns">
-        @component('sponsors.partials.gold_sponsors', ['sponsor' => $sponsor])
-        @endcomponent
-    </div>
-@empty
-<p class="has-text-centerd">{{ __('home.no_sponsor') }}</p>
-@endforelse
+@if($sponsors->has('gold'))
+    @forelse($sponsors->get('gold') as $sponsor)
+        <div class="columns">
+            @component('sponsors.partials.gold_sponsors', ['sponsor' => $sponsor])
+            @endcomponent
+        </div>
+    @empty
+        <p class="has-text-centerd">{{ __('home.no_sponsor') }}</p>
+    @endforelse
+@else
+    <p class="has-text-centered">{{ __('home.no_sponsor') }}</p>
+@endif
 
 
 <h2 class="title is-2 title-perso">
     Silver
 </h2>
 <div class="columns section">
-    @forelse($sponsors->get('silver') as $sponsor)
-        @component('sponsors.partials.silver_sponsors', ['sponsor' => $sponsor])
-        @endcomponent
-    @empty
-    <p class="has-text-centerd">{{ __('home.no_sponsor') }}</p>
-    @endforelse
+    @if($sponsors->has('silver'))
+        @forelse($sponsors->get('silver') as $sponsor)
+            @component('sponsors.partials.silver_sponsors', ['sponsor' => $sponsor])
+            @endcomponent
+        @empty
+            <p class="has-text-centerd">{{ __('home.no_sponsor') }}</p>
+        @endforelse
+    @else
+        <p class="has-text-centered">{{ __('home.no_sponsor') }}</p>
+    @endif
 </div>
 
 
@@ -28,10 +36,14 @@
     Bronze
 </h1>
 <div class="columns">
-    @forelse($sponsors->get('bronze') as $sponsor)
-        @component('sponsors.partials.bronze_sponsors', ['sponsor' => $sponsor])
-        @endcomponent
-    @empty
-    <p class="has-text-centerd">{{ __('home.no_sponsor') }}</p>
-    @endforelse
+    @if($sponsors->has('bronze'))
+        @forelse($sponsors->get('bronze') as $sponsor)
+            @component('sponsors.partials.bronze_sponsors', ['sponsor' => $sponsor])
+            @endcomponent
+        @empty
+            <p class="has-text-centerd">{{ __('home.no_sponsor') }}</p>
+        @endforelse
+    @else
+        <p class="has-text-centered">{{ __('home.no_sponsor') }}</p>
+    @endif
 </div>
