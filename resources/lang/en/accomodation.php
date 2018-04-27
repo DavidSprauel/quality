f5cad5981df5b27483aef8cf859f9c5a0c94d81d
David Sprauel <david@melpia.com>
2018-04-27 11:48:19 +0200

    fix all changes

--- resources/lang/en/accomodation.php
+++ resources/lang/en/accomodation.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    'header_text' => '                    Paris boasts a wide range of accommodation for all budgets and all tastes.',
+    'link_top' => 'Where to sleep in Paris ?',
+    
     'hotel_1_name'    => 'Pullman Paris la défense',
     'hotel_1_address' => '11, avenue de l’Arche',
     'hotel_1_zipcode' => ' 92400',
@@ -21,8 +24,7 @@ For guests arriving by car, the hotel has its own private car park.',
     'hotel_2_zipcode' => ' 75014',
     'hotel_2_city'    => 'Paris',
     'hotel_2_price'   => 'Price from 90€',
-    'hotel_2_desc'    => 'Pullman Paris La Défense is a tranquil, refi ned, bright retreat in
-With a new charter adopted in 2002, the FIAP Jean Monnet reinforced its fundamental commitment of receiving young visitors from around the world. Offering exhibitions, concerts and meetings involving young visitors, the cultural program epitomizes the association\'s commitment. At the FIAP Jean Monnet people from all kinds are given the opportunity to organize seminars, congresses and meetings. A meeting place run by committed professionals and a board of voluntary administrators. The centre promotes all initiatives contributing to the creation of a global citizensh.',
+    'hotel_2_desc'    => 'With a new charter adopted in 2002, the FIAP Jean Monnet reinforced its fundamental commitment of receiving young visitors from around the world. Offering exhibitions, concerts and meetings involving young visitors, the cultural program epitomizes the association\'s commitment. At the FIAP Jean Monnet people from all kinds are given the opportunity to organize seminars, congresses and meetings. A meeting place run by committed professionals and a board of voluntary administrators. The centre promotes all initiatives contributing to the creation of a global citizensh',
 
     'hotel_3_name'    => 'Renaissance Paris La Defense Hotel',
     'hotel_3_address' => '60 Jardin de Valmy',
@@ -57,4 +59,37 @@ A haven of peace and tranquillity at the heart of the hustle and bustle of Paris
 The abundant, well-tended vegetation will make you forget all about the frenzy of city living overnight. Enjoy the view over the indoor garden and the terrace on a sunny day.<br><br>
 
 There can be nothing more exquisite than enjoying a relaxed breakfast on the terrace or continuing your summer evenings in the garden, enjoying a cocktail in a soothing, romantic setting. ',
+
+    'hotel_6_name'    => 'Hotel Ibis Avenue d\'Italie',
+    'hotel_6_address' => '15bis Avenue d\'Italie',
+    'hotel_6_zipcode' => '75013',
+    'hotel_6_city'    => 'Paris',
+    'hotel_6_price'   => '',
+    'hotel_6_desc'    => 'The ibis Paris Avenue d\'Italie 13ème hotel is located 6.2 miles (10 km) from Orly and 109 yards (100 m) from Place d\'Italie, where 3 metro lines connect you to the whole of Paris, from the Eiffel Tower to La Défense, and the Italie Deux shopping center is just a short walk from the hotel. We offer 111 rooms with air conditioning and free WIFI. You can also take advantage of preferential rates at the Club Med Gym, 22 yards (20 m) away.<br><br>
+
+Close to the Austerlitz, Montparnasse and Gare de Lyon train stations, the ibis Paris Avenue d\'Italie enjoys a privileged and easy-to-access location.',
+
+    'hotel_7_name'    => 'Mercure Place d’Italie 4*',
+    'hotel_7_address' => '25 Boulevard Auguste Blanqui',
+    'hotel_7_zipcode' => '75013',
+    'hotel_7_city'    => 'Paris',
+    'hotel_7_price'   => '',
+    'hotel_7_desc'    => 'Located in the heart of Paris, near the Butte aux Cailles district and the Latin Quarter, the Mercure Paris Place d\'Italie hotel offers customized services for your meetings. This hotel provides direct access by metro to Gare Montparnasse and Gare d\'Auste rlitz. It has comfortable rooms with views over the Panthéon and the Eiffel Tower. With family, friends or after a meeting, come and relax in our cozy bar, open 24 hours.<br><br>
+
+By choosing the Paris Place d\'Italie Hotel, you\'ll have the opportunity to stroll through Montparnasse, the Latin Quarter, the Chinese Quarter, and the Butte aux Cailles. You\'ll enjoy all the attractions of the capital.',
+
+    'hotel_8_name'    => 'Kyriad',
+    'hotel_8_address' => '5 Rue Véronèse',
+    'hotel_8_zipcode' => '75013',
+    'hotel_8_city'    => 'Paris',
+    'hotel_8_price'   => '',
+    'hotel_8_desc'    => 'Hotel Kyriad Paris 13 – Italie Gobelins is an excellent base from which to explore the charms of Parisian life, whether you\'re here for a business trip or as a tourist. <br><br>
+
+Located in central Paris, our 3-star hotel offers you all the comforts you need in a quiet and attractive neighborhood (restaurants and movie theatres nearby). <br><br>
+
+At about 100 yards from the Gobelins and Place d\'Italie metro stations (lines 5, 6 and 7), our modern hotel is 5 minutes from Rue Mouffetard and its many restaurants, the Jardin des Plantes and its famous museums (including the Grande Galerie and the Museum of Natural History), and about 160 yards from the Gobelins factory, the Arab World Institute, the Asian Quarter and the François Mitterrand Library. There is also a gym 5 minutes away on foot (Club Med gym, at Place d\'Italie). <br><br>
+
+The Gare d\'Austerlitz and Gare de Lyon train stations are not far, as well as the Butte-aux-Cailles neighborhood and the Pitié-Salpêtrière Hospital. A few metro stations away, you will find Notre-Dame Cathedral and the Panthéon. <br><br>
+
+Our teams at Hotel Kyriad Paris 13 – Italie Gobelins welcome you 24/7, in French or English. Board games are available for your children at the hotel, and we have an elevator. With all this comfort, you will never want to go home.',
 ];
---
+++ resources/lang/fr/accomodation.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+    'header_text' => 'Paris bénéficie d\'un large choix d\'hébérgements pour tout les budgets et pour tout le sgoûts',
+    'link_top' => 'Ou dormir à Paris ?',
+    
+    'hotel_1_name'    => 'Pullman Paris la défense',
+    'hotel_1_address' => '11, avenue de l’Arche',
+    'hotel_1_zipcode' => ' 92400',
+    'hotel_1_city'    => 'Courbevoie (Paris La Défense)',
+    'hotel_1_price'   => 'Prix à partir de 215€',
+    'hotel_1_desc'    => 'Au coeur d\'un des plus grands centres d\'affaires d\'Europe, à 10 minutes des Champs-Elysées, le Pullman Paris La Défense est une parenthèse feutrée et lumineuse au centre de la Défense. Cet hôtel haut de gamme vous propose des chambres design et spacieuses. Savourez de délicieux moments au restaurant Quinte&Sens avec sa cuisine ouverte. Vous pouvez également profiter du bar, du fitness, du WiFi et d\'un parking privé. Les espaces de réunion assureront la réussite de vos séminaires et évènements',
+    
+    'hotel_2_name'    => 'FIAP',
+    'hotel_2_address' => '30 rue Cabanis',
+    'hotel_2_zipcode' => ' 75014',
+    'hotel_2_city'    => 'Paris',
+    'hotel_2_price'   => 'Prix à partir de 90€',
+    'hotel_2_desc'    => 'Le FIAP Jean Monnet est un Centre International de Sejour situé en plein cœur de Paris dans le 14ème arrondissement. Son objectif tel qu’il a été imaginé et conçu à la fin de la Seconde Guerre Mondiale est de favoriser la rencontre et les échanges entre les jeunes et de lutter contre toutes les formes de discriminations.',
+    
+    'hotel_3_name'    => 'Renaissance Paris La Defense Hotel',
+    'hotel_3_address' => '60 Jardin de Valmy',
+    'hotel_3_zipcode' => ' 92918',
+    'hotel_3_city'    => 'Paris La Defense',
+    'hotel_3_price'   => 'Prix à partir de 230€',
+    'hotel_3_desc'    => 'Passez un délicieux séjour parisien au Renaissance Paris La Défense Hôtel. Notre hôtel est situé au cœur du quartier des affaires de la Défense. Nos spacieuses chambres et suites récemment réaménagées vous promettent un séjour chic empreint de raffinement à la française. Profitez des nombreux avantages gratuits de l\'hôtel, notamment l\'accès Internet sans fil dans toutes les chambres et les espaces publics, un centre de remise en forme bien équipé et un personnel prévenant qui se fera un plaisir de vous guider lors de votre séjour. Organisez votre réunion d\'affaires dans notre hôtel de la Défense, et faites votre choix parmi nos 13 salles élégantes récemment réaménagées totalisant près de 700 mètres carrés d\'espace dédié aux événements. Situé à quelques minutes à peine de nombreux sites d\'intérêt tels que les Champs-Élysées, notre hôtel vous place au plus près des trésors de Paris. Que vous voyagiez pour les affaires ou pour les loisirs, le Renaissance Paris La Défense Hôtel vous offre tout ce qu\'il faut pour passer un séjour d\'exception.',
+    
+    'hotel_4_name'    => 'Hôtel Eiffel Blomet',
+    'hotel_4_address' => '78 Rue Blomet',
+    'hotel_4_zipcode' => ' 75015',
+    'hotel_4_city'    => 'Paris',
+    'hotel_4_price'   => 'Prix à partir de 200€',
+    'hotel_4_desc'    => 'A la suite d\'une longue période d\'embellissement, le nouvel Hôtel Eiffel Blomet vous reçoit à deux pas seulement de la Tour Eiffel. Une renaissance qui coïncide avec celle du mythique Bal Blomet voisin qui ré-ouvre également ses portes en ce printemps 2017. Ce dernier, connu à l\'époque sous le nom de "bal Nègre" reçut les icônes magnifiques de ces "années folles" où l\'on dansait le charleston au son d\'un jazz endiablé.<br><br>
+
+Emblématique des années 1930 et de sa vogue Art Déco, l\'Hôtel Eiffel Blomet reprend, orchestré par un groupe de 7 hôteliers confirmés et avec la complicité d\'architectes de talent, la grande tradition de l\'hôtellerie parisienne de l\'époque : un cadre aussi somptueux que confortable et chaleureux mais aussi un service exigeant et distingué.<br><br>
+
+En plus d\'un cadre exceptionnel et de nombreux services, l\'Hôtel Eiffel Blomet bénéficie d\'une situation fabuleuse. Niché au calme au cœur du 15ème arrondissement de Paris, il est également situé à moins d\'une demi-heure à pied du plus emblématique monument de la capitale, la célèbre Tour Eiffel, dont les dentelles de fer dominent la ville-lumière.
+
+',
+    
+    
+    'hotel_5_name'    => 'Hôtel Ampère Paris',
+    'hotel_5_address' => '102 Avenue de Villiers',
+    'hotel_5_zipcode' => '75017',
+    'hotel_5_city'    => 'Paris',
+    'hotel_5_price'   => 'Prix à partir de 230€',
+    'hotel_5_desc'    => 'Au coeur d’une végétation luxuriante et soignée, l’Hôtel Ampère cultive son jardin comme une perle rare et vous fait oublier l’espace d’une nuit la frénésie urbaine. Ajoutez un cadre féerique à cet hôtel de luxe et découvrez notre établissement considéré comme l’un des hôtels 4 étoiles les plus originaux de la capitale.<br><br>
+
+Vous y apprécierez la vue sur le jardin intérieure, la terrasse aux beaux jours et toute l\'équipe à votre service pour que vous passiez un excellent séjour.',
+
+
+    'hotel_6_name'    => 'Hotel Ibis Avenue d\'Italie',
+    'hotel_6_address' => '15bis Avenue d\'Italie',
+    'hotel_6_zipcode' => '75013',
+    'hotel_6_city'    => 'Paris',
+    'hotel_6_price'   => '',
+    'hotel_6_desc'    => 'L\'hôtel Ibis Paris Avenue d\'Italie 13ème est situé à 10 kms d\'Orly et à 100m de la Place d\'Italie où 3 lignes de métro vous permettent de rejoindre le tout Paris, la Tour Eiffel, la Défense et le Centre commercial Italie 2 au pied de l\'hôtel. Nous vous proposons 111 chambres climatisées et équipées du wifi gratuit. Possibilité de profiter du Club Med Gym à 20m à des tarifs préférentiels.<br><br>
+
+Proche des gares d\'Austerlitz, Montparnasse et de Lyon, l\'hôtel Ibis Paris Avenue d\'Italie bénéficie d\'un emplacement privilégié et facile d\'accès.',
+
+    'hotel_7_name'    => 'Mercure Place d’Italie 4*',
+    'hotel_7_address' => '25 Boulevard Auguste Blanqui',
+    'hotel_7_zipcode' => '75013',
+    'hotel_7_city'    => 'Paris',
+    'hotel_7_price'   => '',
+    'hotel_7_desc'    => 'Situé en plein coeur de Paris, à proximité du quartier de la Butte aux Cailles, de la Station F et du Quartier Latin, l\'hôtel Mercure Paris Place d\'Italie offre des prestations sur-mesure pour l\'organisation de vos réunions. Cet établissement donne un accès direct en métro aux gares Montparnasse et Austerlitz. Il dispose de chambres confortables avec vue sur le Panthéon et la Tour Eiffel. En famille, entre amis ou après une réunion, venez-vous détendre dans notre bar cosy ouvert 24/ 24h.<br><br>
+
+Le Mercure Paris Place d\'Italie vous offre la possibilité de flâner dans les quartiers de la Butte aux Cailles, Montparnasse, Latin ou Chinois. Vous pouvez profiter de tous les attraits de la Capitale, grâce à un accès direct en métro (lignes 5-6-7).',
+
+    'hotel_8_name'    => 'Kyriad',
+    'hotel_8_address' => '5 Rue Véronèse',
+    'hotel_8_zipcode' => '75013',
+    'hotel_8_city'    => 'Paris',
+    'hotel_8_price'   => '',
+    'hotel_8_desc'    => 'L\'hôtel Kyriad Paris 13 – Italie Gobelins est un excellent point de départ pour profiter des charmes de la vie parisienne, que vous soyez là pour un voyage d\'affaires ou pour visiter la ville.<br><br>
+
+Situé au centre de Paris, notre hôtel 3 étoiles vous offrira tout le confort nécessaire au cœur d\'un quartier calme et attractif (restaurants et cinémas à proximité).<br><br>
+
+À 100 m des métros Les Gobelins et Place d\'Italie (lignes 5, 6 et 7), notre hôtel moderne est à 5 min à pied de la rue Mouffetard et de ses nombreux restaurants, du Jardin des Plantes et de ses célèbres musées (dont la Grande galerie et le Muséum d\'histoire naturelle), et à 150 m de la manufacture des Gobelins, de l\'Institut du Monde arabe, du quartier chinois et de la bibliothèque François Mitterrand. Une salle de sport est également disponible à 5 min à pied (le Club Med gym, place d\'Italie).<br><br>
+
+La gare d\'Austerlitz et la gare de Lyon ne sont pas loin, de même que les sites de la Butte-aux-Cailles et du groupe hospitalier de la Pitié-Salpêtrière. À quelques stations de métro, vous trouverez la Cathédrale Notre-Dame et le Panthéon.<br><br>
+
+Les équipes de l\'hôtel Kyriad Paris 13 – Italie Gobelins vous accueille 24h/24, en français comme en anglais. Des jeux de société pour vos enfants sont disponibles à l\'hôtel, et l\'hôtel possède un ascenseur. Avec un tel confort, vous aurez du mal à nous quitter.',
+];
